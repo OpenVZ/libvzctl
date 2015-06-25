f9cbff114fe774491bcfcc45beb223d5a02799bd
Igor Sukhih <igor@parallels.com>
2015-06-25 08:41:34 -0400

    Unable to mount ploop inside CT
    Temporary give write permission to ploop device
    
    Signed-off-by: Igor Sukhih <igor@parallels.com>

--- lib/disk.c
+++ lib/disk.c
@@ -777,7 +777,7 @@ static int configure_devperm(struct vzctl_env_handle *h, struct vzctl_disk *disk
 {
 	struct vzctl_dev_perm devperms = {
 		.dev = dev,
-		.mask = S_IROTH | S_IXUSR, /* Set ro | ACC_MOUNT */
+		.mask = S_IROTH | S_IWOTH, /* set S_IWOTH as workaround for #PSBM-34497 */
 		.type = S_IFBLK | VE_USE_MINOR,
 	};
 
