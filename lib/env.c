OpenVZ/libvzctl

commit 872084a835896b4f12511115f5cde34133d0b6ae

vzctl2_env_set_veth_param: access to pinter before check
memcpy(&tmp, param, size);
if (param == NULL || tmp.dev_name_ve == NULL)
	return VZCTL_E_INVAL;

Signed-off-by: Igor Sukhih <igor@parallels.com>

lib/env.c
@@ -1904,10 +1904,10 @@ int vzctl2_env_set_veth_param(struct vzctl_veth_dev *dev, struct vzctl_veth_dev_
 	struct vzctl_ip_param *ip = NULL;
 	struct vzctl_veth_dev_param tmp = {};
 
-	memcpy(&tmp, param, size);
-
 	if (param == NULL || tmp.dev_name_ve == NULL)
 		return VZCTL_E_INVAL;
+
+	memcpy(&tmp, param, size);
 
 	if (strlen(tmp.dev_name_ve) >= IFNAMSIZE)
 		return VZCTL_E_INVAL;

commit e9512a1a0ad28b82ada49beead6a90a454715e38

vzctl2_env_set_veth_param(): failed, #PSBM-34648
broken by 872084a8, fix parameter check

Signed-off-by: Igor Sukhih <igor@parallels.com>

lib/env.c
@@ -1912,7 +1912,7 @@ int vzctl2_env_set_veth_param(struct vzctl_veth_dev *dev, struct vzctl_veth_dev_
 	struct vzctl_ip_param *ip = NULL;
 	struct vzctl_veth_dev_param tmp = {};
 
-	if (param == NULL || tmp.dev_name_ve == NULL)
+	if (param == NULL || param->dev_name_ve == NULL)
 		return VZCTL_E_INVAL;
 
 	memcpy(&tmp, param, size);