f536c48ce8f5d0210d066927eaa80d8114a3366b
Igor Sukhih <igor@parallels.com>
2015-07-21 05:05:13 -0400

    vzctl2_env_is_mounted: Use root disk to detect CT mounted state
    Signed-off-by: Igor Sukhih <igor@parallels.com>

--- lib/vz.c
+++ lib/vz.c
@@ -339,7 +339,10 @@ int vzctl2_env_is_mounted(struct vzctl_env_handle *h)
 	if (ve_private != NULL &&
 			vzctl2_env_layout_version(ve_private) == VZCTL_LAYOUT_5)
 	{
-		return vzctl2_is_image_mounted(get_root_disk(h));
+		struct vzctl_disk *root = find_root_disk(h->env_param->disk);
+
+		if (root->path != NULL)
+			return vzctl2_is_image_mounted(root->path);
 	}
 
 	return fs_is_mounted_check_by_dev(target);
