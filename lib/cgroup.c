OpenVZ/libvzctl

commit 0032828d1a90b34cb3ea4573288c71a7f59f4ebe

Fixed compilation
cgroup.c: In function 'create_perctl_symlink':
cgroup.c:883:8: error: ignoring return value of 'getcwd', declared with attribute warn_unused_result [-Werror=unused-result]
  getcwd(cwd, sizeof(cwd));
        ^
cgroup.c:910:7: error: ignoring return value of 'chdir', declared with attribute warn_unused_result [-Werror=unused-result]
  chdir(cwd);

Signed-off-by: Igor Sukhih <igor@parallels.com>

lib/cgroup.c
@@ -880,7 +880,8 @@ static int create_perctl_symlink(const char *root, const char *path)
 	if (p == NULL)
 		return 0;
 
-	getcwd(cwd, sizeof(cwd));
+	if (getcwd(cwd, sizeof(cwd)))
+		return vzctl_err(-1, errno, "Cannot getcwd");
 
 	snprintf(d, sizeof(d), "%s/%s/..", root, path);
 	if (chdir(d))
@@ -907,7 +908,8 @@ static int create_perctl_symlink(const char *root, const char *path)
 			last = 1;
 	}
 
-	chdir(cwd);
+	if (chdir(cwd))
+		logger(-1, errno, "Cannot cndir %s", cwd);
 
 	return ret;
 }

commit ac3b40b19a11a50e43e192d14e3cfa64716ca760

Failed to start CT: broken by 0032828d
fixed the getcwd() result check

Signed-off-by: Igor Sukhih <igor@parallels.com>

lib/cgroup.c
@@ -880,7 +880,7 @@ static int create_perctl_symlink(const char *root, const char *path)
 	if (p == NULL)
 		return 0;
 
-	if (getcwd(cwd, sizeof(cwd)))
+	if (getcwd(cwd, sizeof(cwd)) == NULL)
 		return vzctl_err(-1, errno, "Cannot getcwd");
 
 	snprintf(d, sizeof(d), "%s/%s/..", root, path);