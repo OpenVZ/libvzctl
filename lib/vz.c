OpenVZ/libvzctl

commit 06bc4e8c5db35adf55b7285bc12a366f71f6b00e

Reduce number of stat() on vzctl2_get_env_status_info() call
removed ecess stat() calls

Signed-off-by: Igor Sukhih <igor@parallels.com>

lib/vz.c
@@ -419,13 +419,10 @@ int vzctl2_get_env_status_info(struct vzctl_env_handle *h,
 
 	vzctl2_get_env_conf_path(EID(h), path, sizeof(path));
 	if (stat_file(path) == 1) {
-		/* get exists state */
-		if (stat_file(ve_private) == 1) {
-			if (mask & ENV_SKIP_OWNER)
-				exists = 1;
-			else if (vzctl2_check_owner(ve_private) == 0)
-				exists = 1;
-		}
+		if (mask & ENV_SKIP_OWNER)
+			exists = 1;
+		else if (vzctl2_check_owner(ve_private) == 0)
+			exists = 1;
 	}
 
 	if ((mask & ENV_STATUS_EXISTS) && exists)
@@ -548,19 +545,15 @@ int vzctl2_env_layout_version(const char *path)
 	if (path == NULL)
 		return 0;
 
-	if (stat(path, &st) != 0) {
-		if (errno != ENOENT)
-			return vzctl_err(-1, errno, "Unable to stat %s", path);
-		return 0;
-	}
-	if (!S_ISDIR(st.st_mode))
-		return vzctl_err(-1, 0, "Unable to get the Container layout: %s is not a"
-				" directory", path);
 	snprintf(buf, sizeof(buf), "%s/" VZCTL_VE_LAYOUT, path);
 	if (lstat(buf, &st) == -1) {
 		if (errno != ENOENT)
-			logger(-1, errno, "Unable to get the Container layout:"
-					" failed to stat %s", buf);
+			return vzctl_err(-1, errno, "Unable to get the"
+				" Container layout: failed to stat %s", buf);
+
+		if (stat(path, &st) && errno != ENOENT)
+			return vzctl_err(-1, errno, "Unable to stat %s", path);
+
 		return 0;
 	}
 	if (!S_ISLNK(st.st_mode))
@@ -750,16 +743,12 @@ static int vzctl_check_owner_quiet(
 	FILE *fp;
 	int ret;
 
-	ret = stat_file(ve_private);
-	if (ret == -1)
-		return VZCTL_E_SYSTEM;
-	else if (ret == 0)
-		return 0;
-
 	ret = is_shared_fs(ve_private);
-	if (ret == -1)
+	if (ret == -1) {
+		if (errno != ENOENT)
+			return 0;
 		return VZCTL_E_SYSTEM;
-	else if (ret == 0)
+	} else if (ret == 0)
 		return 0;
 
 	ret = vzctl2_env_layout_version(ve_private);

lib/cluster.c
@@ -160,7 +160,8 @@ int is_shared_fs(const char *path)
 	struct statfs st;
 
 	if (statfs(path, &st)) {
-		logger(-1, errno, "statfs '%s'", path);
+		if (errno != ENOENT)
+			logger(-1, errno, "statfs '%s'", path);
 		return -1;
 	}
 	return (st.f_type == GFS2_MAGIC ||

lib/config.c
@@ -200,16 +200,13 @@ static int parse_conf_data(struct vzctl_config *conf, const char *fname, int fla
 	char ltoken[4096];
 	char buf[4096 * 10];
 
-	if (!stat_file(fname)) {
-		if (flags & VZCTL_CONF_SKIP_NON_EXISTS)
+	if ((fp = fopen(fname, "r")) == NULL) {
+		if (errno == ENOENT && (flags & VZCTL_CONF_SKIP_NON_EXISTS))
 			return 0;
-		return vzctl_err(VZCTL_E_CONFIG, 0,
-			"Container configuration file '%s' does not exist",
-					fname);
+
+		return vzctl_err(VZCTL_E_CONFIG, errno, "Unable to open %s",
+				fname);
 	}
-	if ((fp = fopen(fname, "r")) == NULL)
-		return vzctl_err(VZCTL_E_CONFIG, errno,
-			"Unable to open %s", fname);
 	debug(DBG_CFG, "parse_conf_data: %s", fname);
 	while (fgets(buf, sizeof(buf), fp) != NULL) {
 		line++;

commit 3eed57da3d06f873247a86835f632f7f22e85908

vzctl_check_owner_quiet: skip check if VE_PRIVATE does not exists #PSBM-42186
broken by commit 06bc4e8c5d "Reduce number of stat() on vzctl2_get_env_status_info() call"

Signed-off-by: Igor Sukhih <igor@parallels.com>

lib/vz.c
@@ -746,7 +746,7 @@ static int vzctl_check_owner_quiet(
 
 	ret = is_shared_fs(ve_private);
 	if (ret == -1) {
-		if (errno != ENOENT)
+		if (errno == ENOENT)
 			return 0;
 		return VZCTL_E_SYSTEM;
 	} else if (ret == 0)