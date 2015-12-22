OpenVZ/libvzctl

commit 074b083712bc8e5f10bef17168d9c7148d28ffe5

Setup sysfs permissions for root disk #PSBM-42369
root disk is mounted from host and do not haved representation in sysfs inside CT.
Systemd do unmount submounts for inactive device.

This fix do setup syfs permission for root image.

Signed-off-by: Igor Sukhih <igor@parallels.com>

lib/disk.c
@@ -892,6 +892,8 @@ static int do_setup_disk(struct vzctl_env_handle *h, struct vzctl_disk *disk,
 	struct stat st;
 	char devname[STR_SIZE];
 	dev_t dev;
+	int skip_configure = (flags & VZCTL_SKIP_CONFIGURE) ||
+						is_root_disk(disk);
 
 	if (disk->use_device) {
 		snprintf(devname, sizeof(devname), "%s", disk->path);
@@ -914,9 +916,11 @@ static int do_setup_disk(struct vzctl_env_handle *h, struct vzctl_disk *disk,
 		/* Give access to the first partition 'ploopNp1' */
 		dev += 1;
 		get_partition_dev_name(dev, part, sizeof(part));
-		ret = get_fs_uuid(part, disk->fsuuid);
-		if (ret)
-			return ret;
+		if (!skip_configure) {
+			ret = get_fs_uuid(part, disk->fsuuid);
+			if (ret)
+				return ret;
+		}
 	}
 
 	ret = configure_mount_opts(h, disk, dev);
@@ -931,7 +935,7 @@ static int do_setup_disk(struct vzctl_env_handle *h, struct vzctl_disk *disk,
 	if (ret)
 		return ret;
 
-	if (!(flags & VZCTL_SKIP_CONFIGURE)) {
+	if (!skip_configure) {
 		ret = configure_disk(h, disk, dev, flags, automount);
 		if (ret)
 			return ret;
@@ -1306,13 +1310,13 @@ int vzctl_setup_disk(struct vzctl_env_handle *h, struct vzctl_env_disk *env_disk
 		return 0;
 
 	list_for_each(disk, &env_disk->disks, list) {
-                if (disk->enabled == VZCTL_PARAM_OFF ||
-				is_root_disk(disk))
-                        continue;
+		if (disk->enabled == VZCTL_PARAM_OFF)
+			continue;
 
 		ret = do_setup_disk(h, disk, flags, 0);
 		if (ret && is_permanent_disk(disk))
 			return ret;
+
 		configured = 1;
 	}
 

lib/disk_configure.c
@@ -294,7 +294,7 @@ static int env_configure_disk(struct exec_disk_param *param)
 	if (send_uevent(device))
 		return -1;
 
-	if (disk->mnt != NULL) {
+	if (disk->mnt != NULL && param->fsuuid != NULL) {
 		if (access(disk->mnt, F_OK))
 			make_dir(disk->mnt, 1);
 
@@ -333,7 +333,7 @@ static struct vzctl_disk *find_disk_by_fsuuid(struct vzctl_env_disk *env_disk, c
 	struct vzctl_disk *disk;
 
 	list_for_each(disk, &env_disk->disks, list)
-		if (!strcmp(disk->fsuuid, fsuuid))
+		if (disk->fsuuid != NULL && !strcmp(disk->fsuuid, fsuuid))
 			return disk;
 
 	return NULL;

commit ecb6ff4fb4fd3050958417854da925b2aa6228a6

CT start failed with enabled 2quota #PSBM-42569
Error writing to file /sys/fs/cgroup/ve/101/ve.mount_opts data='0 182:822769;1 balloon_ino=12,pfcache_csum,,usrjquota=aquota.user,grpjquota=aquota.group,jqfmt=vfsv0,': Argument list too long

broken by commit 074b0837.

Skip ve.mount_opts configure for root image

Signed-off-by: Igor Sukhih <igor@parallels.com>

lib/disk.c
@@ -892,8 +892,8 @@ static int do_setup_disk(struct vzctl_env_handle *h, struct vzctl_disk *disk,
 	struct stat st;
 	char devname[STR_SIZE];
 	dev_t dev;
-	int skip_configure = (flags & VZCTL_SKIP_CONFIGURE) ||
-						is_root_disk(disk);
+	int root = is_root_disk(disk);
+	int skip_configure = (flags & VZCTL_SKIP_CONFIGURE) || root;
 
 	if (disk->use_device) {
 		snprintf(devname, sizeof(devname), "%s", disk->path);
@@ -923,9 +923,11 @@ static int do_setup_disk(struct vzctl_env_handle *h, struct vzctl_disk *disk,
 		}
 	}
 
-	ret = configure_mount_opts(h, disk, dev);
-	if (ret)
-		return ret;
+	if (!root) {
+		ret = configure_mount_opts(h, disk, dev);
+		if (ret)
+			return ret;
+	}
 
 	ret = configure_devperm(h, disk, dev, 0);
 	if (ret)