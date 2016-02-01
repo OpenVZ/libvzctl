OpenVZ/libvzctl

commit 591231bfb0104047030469123e7ca1bab567129e

Failed to add secondary disk by device
$ vzctl set VEID --device-add hdd --device /dev/sda --save
Can't open /sys/class/block/ploop0p1/uevent for writing: No such file or directory

use real partition name.

Signed-off-by: Igor Sukhih <igor@parallels.com>

lib/disk_configure.c
@@ -47,6 +47,8 @@
 
 struct exec_disk_param {
 	const char *fsuuid;
+	const char *device;
+	const char *part;
 	dev_t dev;
 	struct vzctl_disk *disk;
 	int automount;
@@ -265,33 +267,26 @@ err:
 	return err;
 }
 
-void get_partition_dev_name(dev_t dev, char *out, int len)
-{
-	snprintf(out, len, "/dev/ploop%dp1", gnu_dev_minor(dev) >> 4);
-}
-
-int send_uevent(const char *devname)
+int send_uevent(const char *part)
 {
 	char path[PATH_MAX];
 
 	snprintf(path, sizeof(path), "/sys/class/block/%s/uevent",
-			get_devname(devname));
+			get_devname(part));
 
 	return write_data(path, "add");
 }
 
 static int env_configure_disk(struct exec_disk_param *param)
 {
-	char device[STR_SIZE];
 	struct vzctl_disk *disk = param->disk;
-
-	get_partition_dev_name(param->dev, device, sizeof(device));
+	const char *device = param->device;
 
 	if (create_static_dev(device, S_IFBLK | S_IRUSR | S_IWUSR,
 				param->dev))
 		return -1;
 
-	if (send_uevent(device))
+	if (send_uevent(param->part))
 		return -1;
 
 	if (disk->mnt != NULL && param->fsuuid != NULL) {
@@ -311,11 +306,14 @@ static int env_configure_disk(struct exec_disk_param *param)
 	return 0;
 }
 
-int configure_disk(struct vzctl_env_handle *h, struct vzctl_disk *disk, dev_t dev,
+int configure_disk(struct vzctl_env_handle *h, struct vzctl_disk *disk,
+		dev_t dev, const char *device, const char *part,
 		int flags, int automount)
 {
 	struct exec_disk_param param = {
 		.fsuuid = disk->fsuuid,
+		.device = device,
+		.part = part,
 		.dev = dev,
 		.disk = disk,
 		.automount = automount

lib/disk.c
@@ -837,6 +837,25 @@ static int configure_devperm(struct vzctl_env_handle *h, struct vzctl_disk *disk
 	return get_env_ops()->env_set_devperm(h, &devperms);
 }
 
+static int add_sysfs_dir(struct vzctl_env_handle *h, const char *sysfs,
+		const char *devname)
+{
+	char buf[PATH_MAX];
+	char t[PATH_MAX];
+	char *p;
+
+	snprintf(t, sizeof(t), "%s/%s/", sysfs, devname);
+	for (p = strchr(t, '/'); p != NULL; p = strchr(p, '/')) {
+		*p = '\0';
+		snprintf(buf, sizeof(buf), "%s rx", t);
+		if (cg_set_param(EID(h), CG_VE, "ve.sysfs_permissions", buf))
+			return VZCTL_E_DISK_CONFIGURE;
+		*p++ = '/';
+	}
+
+	return 0;
+}
+
 static int add_sysfs_entry(struct vzctl_env_handle *h, const char *sysfs)
 {
 	char path[PATH_MAX];
@@ -941,12 +960,7 @@ static int configure_sysfsperm(struct vzctl_env_handle *h, const char *device,
 	if (cg_set_param(EID(h), CG_VE, "ve.sysfs_permissions", "block rx"))
 		return VZCTL_E_DISK_CONFIGURE;
 
-	snprintf(buf, sizeof(buf), "%s rx", sysfs);
-	if (cg_set_param(EID(h), CG_VE, "ve.sysfs_permissions", buf))
-		return VZCTL_E_DISK_CONFIGURE;
-
-	snprintf(buf, sizeof(buf), "%s/%s rx", sysfs, devname);
-	if (cg_set_param(EID(h), CG_VE, "ve.sysfs_permissions", buf))
+	if (add_sysfs_dir(h, sysfs, devname))
 		return VZCTL_E_DISK_CONFIGURE;
 
 	snprintf(buf, sizeof(buf), "%s/%s", sysfs, devname);
@@ -1017,7 +1031,7 @@ static int do_setup_disk(struct vzctl_env_handle *h, struct vzctl_disk *disk,
 		return ret;
 
 	if (!skip_configure) {
-		ret = configure_disk(h, disk, dev, flags, automount);
+		ret = configure_disk(h, disk, dev, device, part, flags, automount);
 		if (ret)
 			return ret;
 	}

lib/disk.h
@@ -57,9 +57,9 @@ struct vzctl_env_disk {
 };
 
 int is_root_disk(struct vzctl_disk *disk);
-int configure_disk(struct vzctl_env_handle *h, struct vzctl_disk *disk, dev_t dev,
+int configure_disk(struct vzctl_env_handle *h, struct vzctl_disk *disk,
+		dev_t dev, const char *device, const char *part,
 		int flags, int automount);
-
 void free_disk_param(struct vzctl_disk_param *disk);
 void free_disk(struct vzctl_disk *disk);
 void free_env_disk(struct vzctl_env_disk *env_disk);

commit 6cfcd0bfec02cae6203951ec46efa915580efbce

Added hdd was not mounted to specified mount point #PSBM-43706
Mount first partition (insted of device), broken by 591231bf

Signed-off-by: Igor Sukhih <igor@parallels.com>

lib/disk_configure.c
@@ -280,10 +280,12 @@ int send_uevent(const char *part)
 static int env_configure_disk(struct exec_disk_param *param)
 {
 	struct vzctl_disk *disk = param->disk;
-	const char *device = param->device;
 
-	if (create_static_dev(device, S_IFBLK | S_IRUSR | S_IWUSR,
+	if (create_static_dev(param->part, S_IFBLK | S_IRUSR | S_IWUSR,
 				param->dev))
+		return -1;
+
+	if (send_uevent(param->device))
 		return -1;
 
 	if (send_uevent(param->part))
@@ -299,8 +301,10 @@ static int env_configure_disk(struct exec_disk_param *param)
 		if (env_configure_systemd_unit(param->fsuuid, disk->mnt, disk->mnt_opts))
 			return -1;
 
-		if (param->automount && mount(device, disk->mnt, "ext4", 0, NULL))
-			return vzctl_err(-1, errno, "Failed to mount %s",  disk->mnt);
+		if (param->automount &&
+				mount(param->part, disk->mnt, "ext4", 0, NULL))
+			return vzctl_err(-1, errno, "Failed to mount %s %s",
+					param->part, disk->mnt);
 	}
 
 	return 0;