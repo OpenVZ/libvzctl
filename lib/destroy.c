6e4bd3c97b34dcd19b03d57835b4015d9320640a
Pavel Vokhmyanin <pvokhmyanin@virtuozzo.com>
2016-02-20 12:47:16 +0300

    MINOR fix styling in validate_ve_private()

--- lib/destroy.c
+++ lib/destroy.c
@@ -244,28 +244,25 @@ static int umount_all(const char *path)
 	return ret;
 }
 
-static int validate_ve_private(const char *veid, int layout, const char *path)
+static int validate_ve_private(const char *ctid, int layout, const char *path)
 {
-	char buf[PATH_MAX], sveid[STR_SIZE], *ptr;
+	char buf[PATH_MAX], sctid[STR_SIZE], *ptr;
 
 	if (layout > 0)
 		return 0;
 
 	if (realpath(path, buf) == NULL) {
-		if (errno == ENOENT) {
+		if (errno == ENOENT)
 			return 0;
-		} else {
-			logger(-1, errno, "realpath function for path \"%s\" returned error", path);
-			return -1;
-		}
+		return vzctl_err(-1, errno, "realpath function for path \"%s\" returned error", path);
 	}
 
 	// We can skip rindex result verifivation because realpath would return a valid path consisting at least of one "/"
 	// Hence rindex will 100% succeed. If path is invalid realpath would fail itself.
 	ptr = rindex(buf, '/');
-	sprintf(sveid, "/%s", veid);
+	sprintf(sctid, "/%s", ctid);
 
-	if (!strcmp(ptr, sveid))
+	if (!strcmp(ptr, sctid))
 		// Lines are equal -> ve_private ends with "/$VEID" which is considered OK
 		return 0;
 	else
@@ -294,14 +291,13 @@ int vzctl2_env_destroy(struct vzctl_env_handle *h, int flags)
 	ret = validate_ve_private(EID(h), fs->layout, fs->ve_private);
 
 	if (ret) {
-		// "-1" in case realpath of ve_private fails
 		if (ret == -1)
+			/* "-1" in case realpath of ve_private fails */
 			logger(-1, 0, "Container's private area (%s) is invalid.",fs->ve_private);
-		else {
-		// "1" in case we couldn't confirm gparam->ve_private as a container's private area
+		else 
+			/* "1" in case we couldn't confirm gparam->ve_private as a container's private area */
 			logger(-1, 0, "Container's private area (%s) does not resemble a valid container directory."
 					" Container removal is aborted to avoid accidential data loss.",fs->ve_private);
-		}
 		return VZCTL_E_FS_DEL_PRVT;
 	}
 
