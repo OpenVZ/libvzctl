OpenVZ/libvzctl

commit 9e388ccb891d2bf3e06634a82d987c114c4b888c

Failed to dump CT #PSBM-48137
dump action was changed to use criu stages in commit 02179f5.
For vzctl2_cpt_cmd() we should use stagesless schema

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

lib/env_nsops.c
@@ -1278,6 +1278,8 @@ static int ns_env_cpt_cmd(struct vzctl_env_handle *h, int action, int cmd,
 	switch (cmd) {
 	case VZCTL_CMD_KILL:
 		return ns_env_stop_force(h);
+	case VZCTL_CMD_DUMP:
+		return criu_cmd(h, cmd, param, NULL);
 	default:
 		return ns_env_chkpnt(h, cmd, param, flags);
 	}

commit 7fce6fb7716ff1876132c6e924bc413561ddb908

Create image snapshot before resume CT processes #PSBM-48810
Restore original logic  commit 02179f58
     Make env_dump() to wait on criu 'post-dump' unconditionally.
    It will allow to perform ploop snapshot before criu unfreeze.

was broken by 9e388ccb

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

lib/env_nsops.c
@@ -1190,6 +1190,7 @@ static int env_dump(struct vzctl_env_handle *h, int cmd,
 		.pseudosuper_fd = -1,
 	};
 
+	logger(0, 0, "Dumping CT to %s", param->dumpfile);
 	if (pipe(status_p))
 		return vzctl_err(VZCTL_E_PIPE, errno, "Cannot create pipe");
 

lib/snapshot.c
@@ -50,6 +50,7 @@
 #include "snapshot.h"
 #include "config.h"
 #include "disk.h"
+#include "env_ops.h"
 
 #define GET_SNAPSHOT_XML_TMP(buf, ve_private) \
 	snprintf(buf, sizeof(buf), "%s/" SNAPSHOT_XML ".tmp", ve_private);
@@ -364,7 +365,7 @@ int vzctl2_env_create_snapshot(struct vzctl_env_handle *h,
 					sizeof(fname));
 			cpt.dumpfile = fname;
 
-			if (vzctl2_env_chkpnt(h, VZCTL_CMD_DUMP, &cpt, 0)) {
+			if (get_env_ops()->env_chkpnt(h, VZCTL_CMD_DUMP, &cpt, 0)) {
 				logger(-1, 0, "Failed to dump Container");
 				goto err2;
 			}
@@ -396,7 +397,7 @@ int vzctl2_env_create_snapshot(struct vzctl_env_handle *h,
 
 err2:
 	if (run) {
-		if (vzctl2_env_chkpnt(h, VZCTL_CMD_RESUME, &cpt, 1))
+		if (get_env_ops()->env_chkpnt(h, VZCTL_CMD_RESUME, &cpt, 1))
 			vzctl_err(-1, 0, "Failed to resume Container");
 	}
 