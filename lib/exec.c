OpenVZ/libvzctl

commit 2a51f7ee730da4943e57517e629c5b48e333ea5e

Deny enter action if CT in suspending transition state #PSBM-50143
to avoid to stail in FREEZING state

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

lib/exec.c
@@ -551,7 +551,7 @@ static int do_env_exec(struct vzctl_env_handle *h, exec_mode_e exec_mode,
 		execFn fn, void *data, int *data_fd, int timeout,
 		int flags, int stdfd[3])
 {
-	int ret, lfd;
+	int ret;
 	pid_t pid;
 	struct vzctl_cleanup_hook *hook;
 	struct exec_param param = {
@@ -570,8 +570,7 @@ static int do_env_exec(struct vzctl_env_handle *h, exec_mode_e exec_mode,
 		.timeout = timeout,
 	};
 
-	lfd = vzctl2_get_enter_lock(h, VZCTL_LOCK_SH);
-	if (lfd < 0)
+	if (is_enter_locked(h))
 		return VZCTL_E_LOCK;
 
 	ret = real_env_exec_init(&param);
@@ -588,7 +587,6 @@ static int do_env_exec(struct vzctl_env_handle *h, exec_mode_e exec_mode,
 	unregister_cleanup_hook(hook);
 err:
 	real_env_exec_close(&param);
-	vzctl2_release_enter_lock(lfd);
 
 	return ret;
 }
@@ -758,7 +756,7 @@ static int env_exec_pty(struct vzctl_env_handle *h, int exec_mode,
 	int st[2] = {-1, -1};
 	int info[2] = {-1, -1};
 	struct sigaction act = {};
-	int i, lfd;
+	int i;
 	int fd_flags[2];
 
 	for (i = 0; i < 2; i++) {
@@ -768,8 +766,7 @@ static int env_exec_pty(struct vzctl_env_handle *h, int exec_mode,
 					"Unable to get fd%d flags", i);
 	}
 
-	lfd = vzctl2_get_enter_lock(h, VZCTL_LOCK_SH);
-	if (lfd < 0)
+	if (is_enter_locked(h))
 		return VZCTL_E_LOCK;
 
 	if (pipe(in) < 0 || pipe(out) < 0 || pipe(st) < 0 || pipe(info) < 0) {
@@ -896,8 +893,6 @@ err:
 		raw_off();
 out:
 
-	vzctl2_release_enter_lock(lfd);
-
 	for (i = 0; i < 2; i++)
 		fcntl(i, F_SETFL, fd_flags[i]);
 
@@ -949,25 +944,21 @@ static int do_env_exec_fn(struct vzctl_env_handle *h, execFn fn, void *data,
 		int *data_fd, int timeout, int flags)
 {
 	pid_t pid;
-	int ret, lfd;
+	int ret;
 	struct vzctl_cleanup_hook *hook;
 
-	lfd = vzctl2_get_enter_lock(h, VZCTL_LOCK_SH);
-	if (lfd < 0)
+	if (is_enter_locked(h))
 		return VZCTL_E_LOCK;
 
 	ret = get_env_ops()->env_exec_fn(h, fn, data, data_fd, timeout, flags, &pid);
 	if (ret)
-		goto err;
+		return ret;;
 
 	hook = register_cleanup_hook(cleanup_kill_process, (void *) &pid);
 	ret = env_wait(pid, timeout, NULL);
 	unregister_cleanup_hook(hook);
 
-err:
-	vzctl2_release_enter_lock(lfd);
-
-	return ret;
+	return 0;
 }
 
 int vzctl2_env_exec_fn2(struct vzctl_env_handle *h, execFn fn, void *data,

lib/cpt.c
@@ -351,7 +351,7 @@ int vz_env_chkpnt(struct vzctl_env_handle *h, int cmd, struct vzctl_cpt_param *p
 	}
 	if (cmd == VZCTL_CMD_CHKPNT || cmd == VZCTL_CMD_FREEZE) {
 		/* Deny to enter on SUSPEND stage */
-		lfd = vzctl2_get_enter_lock(h, VZCTL_LOCK_EX);
+		lfd = get_enter_lock(h);
 		if (lfd < 0)
 			goto err;
 	}
@@ -430,8 +430,7 @@ err:
 		unlink(dumpfile);
 	if (cpt_fd != -1)
 		close(cpt_fd);
-	if (lfd >= 0)
-		vzctl2_release_enter_lock(lfd);
+	release_enter_lock(lfd);
 
 	free_ip(&ip);
 

lib/env.c
@@ -1023,7 +1023,7 @@ err_pipe:
 int vzctl2_env_chkpnt(struct vzctl_env_handle *h, int cmd,
 		struct vzctl_cpt_param *param, int flags)
 {
-	int ret;
+	int ret, lfd;
 
 	if (!is_env_run(h))
 		return vzctl_err(VZCTL_E_ENV_NOT_RUN, 0, "Container is not running");
@@ -1032,6 +1032,8 @@ int vzctl2_env_chkpnt(struct vzctl_env_handle *h, int cmd,
 		return vzctl2_cpt_cmd(h, VZCTL_CMD_CHKPNT, cmd, param, flags);
 
 	logger(0, 0, "Setting up checkpoint...");
+	/* Deny to enter on SUSPEND stage */
+	lfd = get_enter_lock(h);
 
 	if ((ret = get_env_ops()->env_chkpnt(h, cmd, param, flags)))
 		goto end;
@@ -1049,6 +1051,8 @@ end:
 		vzctl2_send_state_evt(EID(h), VZCTL_ENV_SUSPENDED);
 		logger(0, 0, "Checkpointing completed successfully");
 	}
+
+	release_enter_lock(lfd);
 
 	return ret;
 }

lib/lock.c
@@ -427,7 +427,7 @@ void vzctl2_unlock(int fd, const char *lockfile)
 	}
 }
 
-int vzctl2_get_enter_lock(struct vzctl_env_handle *h, int mode)
+static int do_enter_lock(struct vzctl_env_handle *h, int mode)
 {
 	int fd;
 	char fname[PATH_MAX];
@@ -439,16 +439,37 @@ int vzctl2_get_enter_lock(struct vzctl_env_handle *h, int mode)
 		if (mkdir(VZCTL_ENTER_LOCK_DIR, 0755) && errno != EEXIST)
 			return vzctl_err(-1, errno, "failed to create "VZCTL_ENTER_LOCK_DIR);
 	}
-	fd = vzctl2_lock(get_enter_lock_fname(h, fname, sizeof(fname)), mode, VZCTL_ENTER_WAIT_TM);
+
+	get_enter_lock_fname(h, fname, sizeof(fname));
+	fd = vzctl2_lock(fname, mode, VZCTL_ENTER_WAIT_TM);
 	if (fd < 0)
 		logger(-1, 0, "Unable to lock ENTER operation");
+
 	return fd;
 }
 
-void vzctl2_release_enter_lock(int lockfd)
+void release_enter_lock(int lockfd)
 {
 	if (lockfd >= 0)
 		close(lockfd);
+}
+
+int get_enter_lock(struct vzctl_env_handle *h)
+{
+	return do_enter_lock(h, VZCTL_LOCK_EX);
+}
+
+int is_enter_locked(struct vzctl_env_handle *h)
+{
+	int fd;
+
+	fd = do_enter_lock(h, VZCTL_LOCK_SH | VZCTL_LOCK_NB);
+	if (fd < 0)
+		return 1;
+
+	release_enter_lock(fd);
+
+	return 0;
 }
 
 int vzctl_env_conf_lock(struct vzctl_env_handle *h, int mode)

lib/lock.h
@@ -36,8 +36,9 @@ extern "C" {
 int vzctl_lock(const char *lockfile, int mode, unsigned int timeout);
 void vzctl_unlock(int fd, const char *lockfile);
 
-int vzctl2_get_enter_lock(struct vzctl_env_handle *h, int mode);
-void vzctl2_release_enter_lock(int lockfd);
+int get_enter_lock(struct vzctl_env_handle *h);
+void release_enter_lock(int lockfd);
+int is_enter_locked(struct vzctl_env_handle *h);
 int vzctl_env_conf_lock(struct vzctl_env_handle *h, int mode);
 int vzctl_env_conf_unlock(int fd);
 const char *get_dir_lock_file(const char *dir, char *buf, int size);

lib/snapshot.c
@@ -51,6 +51,7 @@
 #include "config.h"
 #include "disk.h"
 #include "env_ops.h"
+#include "lock.h"
 
 #define GET_SNAPSHOT_XML_TMP(buf, ve_private) \
 	snprintf(buf, sizeof(buf), "%s/" SNAPSHOT_XML ".tmp", ve_private);
@@ -300,7 +301,7 @@ static int copy_snapshot_config(struct vzctl_env_handle *h, const char *from, co
 int vzctl2_env_create_snapshot(struct vzctl_env_handle *h,
 		struct vzctl_snapshot_param *param)
 {
-	int ret, run = 0;
+	int ret, run = 0, lfd = -1;
 	char guid[39];
 	char fname[MAXPATHLEN];
 	char tmp[MAXPATHLEN] = "";
@@ -356,6 +357,7 @@ int vzctl2_env_create_snapshot(struct vzctl_env_handle *h,
 
 	/* store dump & continue */
 	if (run) {
+		lfd = get_enter_lock(h);
 		ret = vzctl2_env_chkpnt(h, VZCTL_CMD_FREEZE, &cpt, 0);
 		if (ret)
 			goto err1;
@@ -382,6 +384,8 @@ int vzctl2_env_create_snapshot(struct vzctl_env_handle *h,
 		if (vzctl2_env_chkpnt(h, VZCTL_CMD_RESUME, &cpt, 0))
 			ret = vzctl_err(VZCTL_E_CREATE_SNAPSHOT, 0,
 					"Failed to resume Container");
+
+		release_enter_lock(lfd);
 	}
 
 	// move snapshot.xml to its place
@@ -399,6 +403,8 @@ err2:
 	if (run) {
 		if (get_env_ops()->env_chkpnt(h, VZCTL_CMD_RESUME, &cpt, 1))
 			vzctl_err(-1, 0, "Failed to resume Container");
+
+		release_enter_lock(lfd);
 	}
 
 err1:
@@ -475,7 +481,7 @@ static int restore_env_config(struct vzctl_env_handle *h, const char *guid,
 int vzctl2_env_switch_snapshot(struct vzctl_env_handle *h,
 		struct vzctl_switch_snapshot_param *param)
 {
-	int ret, run;
+	int ret, run, lfd = -1;;
 	char fname[MAXPATHLEN];
 	char snap_xml_tmp[MAXPATHLEN];
 	char ve_conf_tmp[MAXPATHLEN] = "";
@@ -543,6 +549,7 @@ int vzctl2_env_switch_snapshot(struct vzctl_env_handle *h,
 
 	/* freeze */
 	if (run) {
+		lfd = get_enter_lock(h);
 		ret = vzctl2_env_chkpnt(h, VZCTL_CMD_FREEZE, &cpt, 0);
 		if (ret)
 			goto err1;
@@ -590,6 +597,7 @@ int vzctl2_env_switch_snapshot(struct vzctl_env_handle *h,
 		vzctl2_delete_snapshot(h_env_snap, guid_tmp);
 
 	vzctl2_env_close(h_env_snap);
+	release_enter_lock(lfd);
 	logger(0, 0, "Container has been successfully switched "
 			"to %s snapshot", guid);
 
@@ -616,6 +624,7 @@ err1:
 	unlink(ve_conf_tmp);
 
 err:
+	release_enter_lock(lfd);
 	logger(-1, 0, "Failed to switch to snapshot %s", guid);
 	vzctl_free_snapshot_tree(tree);
 

commit c02b6fe4abe9940c14ec93c80deba86dfb5d963e

Failed to resume CT with enable 2nd quota #PSBM-50819
broken by commit 2a51f7ee

"Unable to lock ENTER operation"
"Failed to setup 2nd level quota"

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

lib/exec.c
@@ -947,7 +947,7 @@ static int do_env_exec_fn(struct vzctl_env_handle *h, execFn fn, void *data,
 	int ret;
 	struct vzctl_cleanup_hook *hook;
 
-	if (is_enter_locked(h))
+	if (!(flags & VE_SKIPLOCK) && is_enter_locked(h))
 		return VZCTL_E_LOCK;
 
 	ret = get_env_ops()->env_exec_fn(h, fn, data, data_fd, timeout, flags, &pid);