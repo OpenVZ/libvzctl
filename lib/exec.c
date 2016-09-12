OpenVZ/libvzctl

commit fb92eb8f3e0f812c8e15cb203de336106bd03027

style: remove unused ve_root argunent
  vzctl2_wrap_env_exec_script
  vzctl2_wrap_env_exec_vzscript

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

lib/exec.c
@@ -1095,7 +1095,7 @@ static char **build_arg(char **a, char *const *b)
 	return ar;
 }
 
-static int do_wrap_env_exec_script(struct vzctl_env_handle *h, const char *ve_root,
+static int do_wrap_env_exec_script(struct vzctl_env_handle *h,
 	char *const argv[], char *const envp[], const char *fname,
 	int timeout, int flags,  int use_vz_func, int *retcode)
 {
@@ -1109,7 +1109,7 @@ static int do_wrap_env_exec_script(struct vzctl_env_handle *h, const char *ve_ro
 	argv_param[0] = VZCTL_EXEC_WRAP_BIN;
 	argv_param[1] = (h == NULL) ?  "" : EID(h);
 	argv_param[2] = (char *)fname;
-	argv_param[3] = (char *)ve_root;
+	argv_param[3] = NULL;
 	snprintf(timeout_str, sizeof(timeout_str), "%d", timeout);
 	argv_param[4] = timeout_str;
 	argv_param[5] = use_vz_func ? "1" : "0";
@@ -1140,27 +1140,27 @@ static int do_wrap_env_exec_script(struct vzctl_env_handle *h, const char *ve_ro
 	return env_wait(pid, 0, retcode);
 }
 
-int vzctl2_wrap_env_exec_script(struct vzctl_env_handle *h, const char *ve_root,
+int vzctl2_wrap_env_exec_script(struct vzctl_env_handle *h,
 	char *const argv[], char *const envp[], const char *fname,
 	int timeout, int flags)
 {
-	return do_wrap_env_exec_script(h, ve_root, argv, envp, fname, timeout, flags, 0, NULL);
+	return do_wrap_env_exec_script(h, argv, envp, fname, timeout, flags, 0, NULL);
 }
 
-int vzctl2_wrap_env_exec_vzscript(struct vzctl_env_handle *h, const char *ve_root,
+int vzctl2_wrap_env_exec_vzscript(struct vzctl_env_handle *h,
 	char *const argv[], char *const envp[], const char *fname,
 	int timeout, int flags)
 {
 	if (vzctl2_get_flags() & VZCTL_FLAG_DONT_USE_WRAP)
 		return vzctl2_env_exec_script(h, argv, envp,
-				fname, DIST_FUNC, timeout, flags); 
-	return do_wrap_env_exec_script(h, ve_root, argv, envp, fname,
+				fname, DIST_FUNC, timeout, flags);
+	return do_wrap_env_exec_script(h, argv, envp, fname,
 			timeout, flags, 1, NULL);
 }
 
 int vzctl2_wrap_exec_script_rc(char *const argv[], char *const env[], int flags, int *retcode)
 {
-	return do_wrap_env_exec_script(NULL, "", argv, env, argv[0], 0, flags, 0, retcode);
+	return do_wrap_env_exec_script(NULL, argv, env, argv[0], 0, flags, 0, retcode);
 }
 
 int vzctl2_wrap_exec_script(char *const argv[], char *const env[], int flags)

lib/create.c
@@ -758,8 +758,7 @@ int custom_configure(struct vzctl_env_handle *h, const char *script)
 	logger(0, 0, "Running custom configuration script");
 	snprintf(buf, sizeof(buf), "VEID=%s", EID(h));
 
-	ret = vzctl2_wrap_env_exec_script(h, h->env_param->fs->ve_root,
-			arg, env, script, 0, 0);
+	ret = vzctl2_wrap_env_exec_script(h, arg, env, script, 0, 0);
 	if (ret)
 		logger(-1, 0, "Custom configuration script"
 				" returned with error");

lib/env.c
@@ -244,8 +244,6 @@ static int do_env_post_stop(struct vzctl_env_handle *h, int flags)
 int vzctl2_env_stop(struct vzctl_env_handle *h, stop_mode_e stop_mode, int flags)
 {
 	int ret;
-	struct vzctl_env_param *env = h->env_param;
-	const char *ve_root = env->fs->ve_root;
 	struct vzctl_env_status env_status = {};
 
 	vzctl2_get_env_status_info(h, &env_status, ENV_STATUS_RUNNING);
@@ -274,7 +272,7 @@ int vzctl2_env_stop(struct vzctl_env_handle *h, stop_mode_e stop_mode, int flags
 
 		get_action_script_path(h, VZCTL_STOP_PREFIX, buf, sizeof(buf));
 		if (stat_file(buf) &&
-		    vzctl2_wrap_env_exec_script(h, ve_root, NULL, NULL, buf, 0, EXEC_LOG_OUTPUT))
+		    vzctl2_wrap_env_exec_script(h, NULL, NULL, buf, 0, EXEC_LOG_OUTPUT))
 		{
 			return vzctl_err(VZCTL_E_ACTIONSCRIPT, 0,
 				"Error executing stop script %s", buf);
@@ -876,7 +874,6 @@ int vzctl2_env_start(struct vzctl_env_handle *h, int flags)
 {
 	int ret;
 	struct vzctl_env_param *env = h->env_param;
-	const char *ve_root;
 	struct start_param param = {
 		.h = h,
 		.pseudosuper_fd = -1,
@@ -899,8 +896,6 @@ int vzctl2_env_start(struct vzctl_env_handle *h, int flags)
 	ret = check_requires(env, flags);
 	if (ret)
 		return ret;
-
-	ve_root = env->fs->ve_root;
 
 	if (pipe(h->ctx->wait_p) < 0)
 		return vzctl_err(VZCTL_E_PIPE, errno, "Cannot create pipe");
@@ -951,7 +946,7 @@ int vzctl2_env_start(struct vzctl_env_handle *h, int flags)
 
 		get_action_script_path(h, VZCTL_START_PREFIX, buf, sizeof(buf));
 		if (stat_file(buf) &&
-			vzctl2_wrap_env_exec_script(h, ve_root, NULL, NULL, buf, 0, EXEC_LOG_OUTPUT))
+			vzctl2_wrap_env_exec_script(h, NULL, NULL, buf, 0, EXEC_LOG_OUTPUT))
 		{
 			ret = vzctl_err(VZCTL_E_ACTIONSCRIPT, 0, "Error executing"
 					" start script %s", buf);
@@ -1103,7 +1098,6 @@ int vzctl2_env_restore(struct vzctl_env_handle *h, struct vzctl_cpt_param *param
 {
 	int ret;
 	struct vzctl_env_param *env = h->env_param;
-	const char *ve_root = env->fs->ve_root;
 	struct start_param start_param = {
 		.h = h,
 		.pseudosuper_fd = -1,
@@ -1169,7 +1163,7 @@ int vzctl2_env_restore(struct vzctl_env_handle *h, struct vzctl_cpt_param *param
 
 		get_action_script_path(h, VZCTL_START_PREFIX, buf, sizeof(buf));
 		if (stat_file(buf) &&
-			vzctl2_wrap_env_exec_script(h, ve_root, NULL, NULL, buf, 0, EXEC_LOG_OUTPUT))
+			vzctl2_wrap_env_exec_script(h, NULL, NULL, buf, 0, EXEC_LOG_OUTPUT))
 		{
 			ret = vzctl_err(VZCTL_E_ACTIONSCRIPT, 0, "Error executing"
 					" start script %s", buf);

lib/env_configure.c
@@ -142,8 +142,8 @@ int env_hostnm_configure(struct vzctl_env_handle *h, struct vzctl_env_param *env
 	}
 	envp[i] = NULL;
 	logger(0, 0, "Set hostname: %s", hostname);
-	ret = vzctl2_wrap_env_exec_vzscript(h, h->env_param->fs->ve_root,
-			NULL, envp, script, VZCTL_SCRIPT_EXEC_TIMEOUT, EXEC_LOG_OUTPUT);
+	ret = vzctl2_wrap_env_exec_vzscript(h, NULL, envp, script,
+			VZCTL_SCRIPT_EXEC_TIMEOUT, EXEC_LOG_OUTPUT);
 
 	return ret;
 }
@@ -193,8 +193,8 @@ int env_dns_configure(struct vzctl_env_handle *h, struct vzctl_env_param *env, i
 			envp[i++] = str;
 	}
 	envp[i] = NULL;
-	ret = vzctl2_wrap_env_exec_vzscript(h, h->env_param->fs->ve_root,
-			NULL, envp, script, VZCTL_SCRIPT_EXEC_TIMEOUT, EXEC_LOG_OUTPUT);
+	ret = vzctl2_wrap_env_exec_vzscript(h, NULL, envp, script,
+			VZCTL_SCRIPT_EXEC_TIMEOUT, EXEC_LOG_OUTPUT);
 
 	logger(0, 0, "File resolv.conf was modified");
 	free_ar_str(envp);
@@ -229,8 +229,8 @@ int env_pw_configure(struct vzctl_env_handle *h, const char *user,
 		env[i++] = "IS_CRYPTED=1";
 	env[i] = NULL;
 
-	ret = vzctl2_wrap_env_exec_vzscript(h, h->env_param->fs->ve_root,
-			NULL, env, script, VZCTL_SCRIPT_EXEC_TIMEOUT, EXEC_LOG_OUTPUT);
+	ret = vzctl2_wrap_env_exec_vzscript(h, NULL, env, script,
+			VZCTL_SCRIPT_EXEC_TIMEOUT, EXEC_LOG_OUTPUT);
 
 	if (ret)
 		return vzctl_err(VZCTL_E_CHANGEPASS, 0,
@@ -335,8 +335,8 @@ static int env_quota_configure(struct vzctl_env_handle *h, unsigned long ugidlim
 	envp[i] = NULL;
 
 	logger(0, 0, "Setting quota ugidlimit: %lu", ugidlimit);
-	ret = vzctl2_wrap_env_exec_vzscript(h, h->env_param->fs->ve_root,
-			NULL, envp, h->dist_actions->set_ugid_quota,
+	ret = vzctl2_wrap_env_exec_vzscript(h, NULL, envp,
+			h->dist_actions->set_ugid_quota,
 			VZCTL_SCRIPT_EXEC_TIMEOUT, EXEC_LOG_OUTPUT);
 
 	free_ar_str(envp);
@@ -383,8 +383,8 @@ int env_console_configure(struct vzctl_env_handle *h, int flags)
 	if (h->dist_actions->set_console == NULL)
 		return 0;
 
-	if (vzctl2_wrap_env_exec_vzscript(h, h->env_param->fs->ve_root,
-				NULL, NULL, h->dist_actions->set_console,
+	if (vzctl2_wrap_env_exec_vzscript(h, NULL, NULL,
+				h->dist_actions->set_console,
 				VZCTL_SCRIPT_EXEC_TIMEOUT, EXEC_LOG_OUTPUT))
 		return vzctl_err(VZCTL_E_ACTIONSCRIPT, 0,
 				"Failed to configure Container console");
@@ -435,8 +435,8 @@ int env_ip_configure(struct vzctl_env_handle *h, int cmd,
 	env[i++] = ip_str;
 	env[i] = NULL;
 
-	ret = vzctl2_wrap_env_exec_vzscript(h, h->env_param->fs->ve_root,
-			NULL, env, script, VZCTL_SCRIPT_EXEC_TIMEOUT, EXEC_LOG_OUTPUT);
+	ret = vzctl2_wrap_env_exec_vzscript(h, NULL, env, script,
+			VZCTL_SCRIPT_EXEC_TIMEOUT, EXEC_LOG_OUTPUT);
 
 	free(ip_str);
 

lib/exec.h
@@ -86,10 +86,10 @@ int vzctl2_env_exec_script(struct vzctl_env_handle *h,
 	char *const argv[], char *const envp[], const char *fname,
 	const char *inc, int timeout, int flags);
 
-int vzctl2_wrap_env_exec_script(struct vzctl_env_handle *h, const char *ve_root,
+int vzctl2_wrap_env_exec_script(struct vzctl_env_handle *h,
         char *const argv[], char *const envp[], const char *fname, int timeout, int flags);
 
-int vzctl2_wrap_env_exec_vzscript(struct vzctl_env_handle *h, const char *ve_root,
+int vzctl2_wrap_env_exec_vzscript(struct vzctl_env_handle *h,
         char *const argv[], char *const envp[], const char *fname, int timeout, int flags);
 
 int vzctl2_wrap_exec_script_rc(char *const argv[], char *const env[], int flags, int *retcode);

lib/veth.c
@@ -726,8 +726,8 @@ static int env_veth_configure(struct vzctl_env_handle *h, int add,
 			} else if (!changed)
 				break;
 
-			ret = vzctl2_wrap_env_exec_vzscript(h, h->env_param->fs->ve_root,
-					NULL, env, script, VZCTL_SCRIPT_EXEC_TIMEOUT, EXEC_LOG_OUTPUT);
+			ret = vzctl2_wrap_env_exec_vzscript(h, NULL, env,
+				script, VZCTL_SCRIPT_EXEC_TIMEOUT, EXEC_LOG_OUTPUT);
 			if (ret) {
 				logger(-1, 0, "veth network configuration"
 						" script exited with error %d", ret);

commit f5ad4fc19ace0b33b44c920dc80298a84584d885

do_wrap_env_exec_script: failed to set ip address on running CT
broken by commit fb92eb8f3

Signe-off-by: Igor Sukhih <igor@virtuozzo.com>

lib/exec.c
@@ -1109,7 +1109,7 @@ static int do_wrap_env_exec_script(struct vzctl_env_handle *h,
 	argv_param[0] = VZCTL_EXEC_WRAP_BIN;
 	argv_param[1] = (h == NULL) ?  "" : EID(h);
 	argv_param[2] = (char *)fname;
-	argv_param[3] = NULL;
+	argv_param[3] = "";
 	snprintf(timeout_str, sizeof(timeout_str), "%d", timeout);
 	argv_param[4] = timeout_str;
 	argv_param[5] = use_vz_func ? "1" : "0";