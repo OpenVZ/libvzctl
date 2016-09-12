OpenVZ/libvzctl

commit ffbcc74ca26a31ef826189a1c2e251611eb14a54

Add VZCTL_CONF_LOAD_DISK_INFO flag on parse CT configuration
To fill encryption keyid

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

lib/disk.c
@@ -76,6 +76,7 @@ void free_disk(struct vzctl_disk *disk)
 	free(disk->devname);
 	free(disk->partname);
 	free(disk->dmname);
+	free(disk->enc_keyid);
 	free(disk);
 }
 
@@ -259,6 +260,48 @@ const char *get_root_disk(struct vzctl_env_handle *h)
 	return NULL;
 }
 
+static int load_disk_info(struct vzctl_env_param *env)
+{
+	struct vzctl_disk *disk;
+	struct ploop_disk_images_data *di = NULL;
+	int ret = 0;
+	char path[PATH_MAX];
+
+	if (env->fs->layout < VZCTL_LAYOUT_5)
+		return 0;
+
+	list_for_each(disk, &env->disk->disks, list) {
+		if (disk->use_device)
+			continue;
+
+		get_abs_path(env->fs->ve_private, disk->path, path, sizeof(path));
+		if ((ret = open_dd(path, &di)) ||	(ret = read_dd(di))) {
+			if (!is_permanent_disk(disk))
+				break;
+
+			if (di != NULL) {
+				ploop_close_dd(di);
+				di = NULL;
+			}
+			continue;
+		}
+
+		if (di->enc->keyid != NULL) {
+			ret = xstrdup(&disk->enc_keyid, di->enc->keyid);
+			if (ret)
+				break;
+		}
+
+		ploop_close_dd(di);
+		di = NULL;
+	}
+
+	if (di)
+		ploop_close_dd(di);
+
+	return ret;
+}
+
 int set_disk_param(struct vzctl_env_param *env, int flags)
 {
 	int ret;
@@ -275,15 +318,13 @@ int set_disk_param(struct vzctl_env_param *env, int flags)
 		ret = xstrdup(&env->fs->ve_private_fs, path);
 		if (ret)
 			return ret;
-	} else if (env->disk->root != VZCTL_PARAM_OFF &&	
+	} else if (env->disk->root != VZCTL_PARAM_OFF &&
 			find_root_disk(env->disk) == NULL)
 	{
 		/* build default root disk: VE_PRIVATE/root.hdd */
 		root = calloc(1, sizeof(struct vzctl_disk));
-		if (root == NULL) {
-			ret = vzctl_err(VZCTL_E_NOMEM, ENOMEM, "set_disk_param");
-			goto err;
-		}
+		if (root == NULL)
+			return vzctl_err(VZCTL_E_NOMEM, ENOMEM, "set_disk_param");
 
 		strncpy(root->uuid, DISK_ROOT_UUID, sizeof(root->uuid));
 		root->enabled = VZCTL_PARAM_ON;
@@ -291,9 +332,8 @@ int set_disk_param(struct vzctl_env_param *env, int flags)
 		if (env->dq->diskspace != NULL)
 			root->size = env->dq->diskspace->b;
 
-		ret = xstrdup(&root->path,
-				(flags & VZCTL_CONF_USE_RELATIVE_PATH) ? VZCTL_VE_ROOTHDD_DIR :
-					 get_root_disk_path(env->fs->ve_private, path, sizeof(path)));
+		ret = xstrdup(&root->path, get_root_disk_path(
+				env->fs->ve_private, path, sizeof(path)));
 		if (ret)
 			goto err;
 
@@ -318,6 +358,12 @@ int set_disk_param(struct vzctl_env_param *env, int flags)
 	root = find_root_disk(env->disk);
 	if (root != NULL)
 		root->user_quota = get_user_quota_mode(env->dq);
+
+	if (flags & VZCTL_CONF_LOAD_DISK_INFO) {
+		ret = load_disk_info(env);
+		if (ret)
+			return ret;
+	}
 
 	if (!(flags & VZCTL_CONF_USE_RELATIVE_PATH)) {
 		list_for_each(disk, &env->disk->disks, list) {

include/libvzctl.h
@@ -162,6 +162,7 @@ enum vzctl_config_flags {
 	VZCTL_CONF_SKIP_PARAM_ERRORS	= 0x20,
 	VZCTL_CONF_SKIP_PARSE		= 0x40,
 	VZCTL_CONF_RUNTIME_PARAM	= 0x80,
+	VZCTL_CONF_LOAD_DISK_INFO	= 0x100,
 };
 
 /** Exec modes.

lib/disk.h
@@ -52,6 +52,7 @@ struct vzctl_disk {
 	dev_t part_dev;
 	char *dmname;
 	dev_t dm_dev;
+	char *enc_keyid;
 	disk_mount mount;
 	disk_umount umount;
 };

lib/image.c
@@ -62,6 +62,13 @@ int open_dd(const char *path, struct ploop_disk_images_data **di)
 	return 0;
 }
 
+int read_dd(struct ploop_disk_images_data *di)
+{
+	if (ploop_read_dd(di))
+		return vzctl_err(VZCTL_E_PARSE_DD, 0, "Failed to read %s : %s",
+				DISKDESCRIPTOR_XML, ploop_get_last_error());
+	return 0;
+}
 static int read_root_dd(const char *ve_private, struct ploop_disk_images_data **di)
 {
 	char fname[PATH_MAX];

lib/image.h
@@ -31,6 +31,7 @@ struct vzctl_create_image_param;
 
 const char *get_root_disk_path(const char *ve_private, char *buf, int len);
 int open_dd(const char *path, struct ploop_disk_images_data **di);
+int read_dd(struct ploop_disk_images_data *di);
 int get_ploop_type(const char *type);
 int get_ploop_dev(const char *path, char *dev, int d_len, char *part, int p_len);
 int vzctl2_get_ploop_devs(const char *path, char **out[]);

commit 6f9dce5fbb465f9012a3e777fcddd81f04544ba1

set_disk_param: fixed using VZCTL_CONF_USE_RELATIVE_PATH
broken by ffbcc74c
inverted logic

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

lib/disk.c
@@ -332,8 +332,7 @@ int set_disk_param(struct vzctl_env_param *env, int flags)
 		if (env->dq->diskspace != NULL)
 			root->size = env->dq->diskspace->b;
 
-		ret = xstrdup(&root->path, get_root_disk_path(
-				env->fs->ve_private, path, sizeof(path)));
+		ret = xstrdup(&root->path, VZCTL_VE_ROOTHDD_DIR);
 		if (ret)
 			goto err;
 