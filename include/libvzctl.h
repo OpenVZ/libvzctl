OpenVZ/libvzctl

commit 9a77f53dda85fcdedeeffdd341084782e3093726

Get ploop disk stats by path #PSBM-70857
It will aloow to use cached config

int vzctl2_get_disk_stats(const char *path struct vzctl_disk_stats *stats)

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

include/libvzctl.h
@@ -1018,6 +1018,7 @@ int vzctl2_env_attach_disk(struct vzctl_env_handle *h, struct vzctl_disk_param *
 int vzctl2_env_del_disk(struct vzctl_env_handle *h, const char *uuid, int flags);
 int vzctl2_env_detach_disk(struct vzctl_env_handle *h, const char *guid);
 int vzctl2_env_set_disk(struct vzctl_env_handle *h, struct vzctl_disk_param *param);
+int vzctl2_get_disk_stats(const char *path struct vzctl_disk_stats *stats);
 int vzctl2_env_get_disk_stats(struct vzctl_env_handle *h, const char *uuid,
 	struct vzctl_disk_stats *stats, int size);
 

lib/disk.c
@@ -1550,8 +1550,7 @@ static int get_disk_iostat(const char *device, struct vzctl_iostat *stat)
 	return 0;
 }
 
-static int get_ploop_disk_stats(const struct vzctl_disk *disk,
-		struct vzctl_disk_stats *stats)
+int vzctl2_get_disk_stats(const char *path struct vzctl_disk_stats *stats)
 {
 	int ret;
 	char buf[PATH_MAX];
@@ -1559,10 +1558,7 @@ static int get_ploop_disk_stats(const struct vzctl_disk *disk,
 	char partname[STR_SIZE];
 	struct ploop_info info;
 
-	if (disk->use_device)
-		return VZCTL_E_INVAL;
-
-	ret = get_ploop_dev(disk->path, devname, sizeof(devname),
+	ret = get_ploop_dev(path, devname, sizeof(devname),
 			partname, sizeof(partname));
 	if (ret == 0) {
 		ret = get_disk_iostat(devname, &stats->io);
@@ -1591,7 +1587,16 @@ static int get_ploop_disk_stats(const struct vzctl_disk *disk,
 		stats->ifree = info.fs_ifree;
 	}
 
-	return (ret == 0 || ret == 1) ? 0 : VZCTL_E_SYSTEM;
+	return 0;
+}
+
+static int get_ploop_disk_stats(const struct vzctl_disk *disk,
+		struct vzctl_disk_stats *stats)
+{
+	if (disk->use_device)
+		return VZCTL_E_INVAL;
+
+	return vzctl2_get_disk_stats(disk->path);
 }
 
 int vzctl2_env_get_disk_stats(struct vzctl_env_handle *h, const char *uuid,

commit 8d2bf177e8bac367113acbac0c9247e1365931ce

Fixed broken compilationn by commit 9a77f53d
Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

include/libvzctl.h
@@ -1018,7 +1018,7 @@ int vzctl2_env_attach_disk(struct vzctl_env_handle *h, struct vzctl_disk_param *
 int vzctl2_env_del_disk(struct vzctl_env_handle *h, const char *uuid, int flags);
 int vzctl2_env_detach_disk(struct vzctl_env_handle *h, const char *guid);
 int vzctl2_env_set_disk(struct vzctl_env_handle *h, struct vzctl_disk_param *param);
-int vzctl2_get_disk_stats(const char *path struct vzctl_disk_stats *stats);
+int vzctl2_get_disk_stats(const char *path, struct vzctl_disk_stats *stats);
 int vzctl2_env_get_disk_stats(struct vzctl_env_handle *h, const char *uuid,
 	struct vzctl_disk_stats *stats, int size);
 

lib/disk.c
@@ -1550,7 +1550,7 @@ static int get_disk_iostat(const char *device, struct vzctl_iostat *stat)
 	return 0;
 }
 
-int vzctl2_get_disk_stats(const char *path struct vzctl_disk_stats *stats)
+int vzctl2_get_disk_stats(const char *path, struct vzctl_disk_stats *stats)
 {
 	int ret;
 	char buf[PATH_MAX];
@@ -1572,13 +1572,13 @@ int vzctl2_get_disk_stats(const char *path struct vzctl_disk_stats *stats)
 		}
 	} else if (ret == 1) {
 		/* Precache data is not ready, avoid mount ploop */
-		snprintf(buf, sizeof(buf), "%s/.statfs", disk->path);
+		snprintf(buf, sizeof(buf), "%s/.statfs", path);
 		if (access(buf, F_OK))
 			return VZCTL_E_INVAL;
 	} else
 		return VZCTL_E_SYSTEM;
 
-	get_dd_path(disk, buf, sizeof(buf));
+	snprintf(buf, sizeof(buf), "%s/" DISKDESCRIPTOR_XML, path);
 	ret = ploop_get_info_by_descr(buf, &info);
 	if (ret == 0) {
 		stats->total = info.fs_bsize * info.fs_blocks / 1024;
@@ -1596,7 +1596,7 @@ static int get_ploop_disk_stats(const struct vzctl_disk *disk,
 	if (disk->use_device)
 		return VZCTL_E_INVAL;
 
-	return vzctl2_get_disk_stats(disk->path);
+	return vzctl2_get_disk_stats(disk->path, stats);
 }
 
 int vzctl2_env_get_disk_stats(struct vzctl_env_handle *h, const char *uuid,