OpenVZ/libvzctl

commit 634896cd1580fa7dfc7a99a9a3b1dc354c5af46f

It not possible to add dm device as secondary disk #PSBM-72010
Update the add device logic to handle dm device with major 253
as single device without partitions
Dont use hardcoded ext4 but query fstype from devisy by means of blkid

NB: it needed kernel support to allow mount XFS inside CT

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

lib/disk.c
@@ -717,8 +717,26 @@ int update_disk_info(struct vzctl_disk *disk)
 	struct stat st;
 
 	if (disk->use_device) {
-		if (get_real_device(disk->path, devname, sizeof(devname)) ||
-				get_part_device(devname, partname, sizeof(partname)))
+		if (get_real_device(disk->path, devname, sizeof(devname)))
+			return VZCTL_E_SYSTEM;
+
+		if (stat(devname, &st))
+			return vzctl_err(VZCTL_E_SYSTEM, errno, "stat %s", devname);
+		if (gnu_dev_major(st.st_rdev) == 253) {
+			disk->dev = st.st_rdev;
+			disk->dm_dev = st.st_rdev;
+			disk->part_dev = st.st_rdev;
+			free(disk->devname);
+			disk->devname = strdup(devname);
+			free(disk->partname);
+			disk->partname = strdup(devname);
+			free(disk->dmname);
+			disk->dmname = strdup(devname);
+			
+			return 0;
+		}
+
+		if (get_part_device(devname, partname, sizeof(partname)))
 			return VZCTL_E_FS_NOT_MOUNTED;
 	} else {
 		char x[STR_SIZE];
@@ -1051,7 +1069,7 @@ static int do_setup_disk(struct vzctl_env_handle *h, struct vzctl_disk *disk,
 	}
 
 	if (!skip_configure && !root) {
-		ret = get_fs_uuid(get_fs_partname(disk), disk->fsuuid);
+		ret = get_fs_uuid(get_fs_partname(disk), disk);
 		if (ret)
 			return ret;
 	}
@@ -1470,7 +1488,9 @@ int vzctl_setup_disk(struct vzctl_env_handle *h, struct vzctl_env_disk *env_disk
 		if (disk->enabled == VZCTL_PARAM_OFF)
 			continue;
 
-		ret = do_setup_disk(h, disk, flags, 0);
+		int automount = disk->dmname ? 1 : 0;
+
+		ret = do_setup_disk(h, disk, flags, automount);
 		if (ret && is_permanent_disk(disk))
 			return ret;
 

lib/disk.h
@@ -32,6 +32,9 @@
 typedef int (*disk_mount)(struct vzctl_env_handle *h, struct vzctl_disk *d, int flags);
 typedef int (*disk_umount)(struct vzctl_disk *d);
 
+typedef char fsuuid_t[37];
+typedef char fstype_t[5];
+
 struct vzctl_disk {
 	list_elem_t list;
 	char uuid[39];
@@ -43,7 +46,8 @@ struct vzctl_disk {
 	int mnt_flags;
 	int user_quota;
 	int autocompact;
-	char fsuuid[39];
+	fsuuid_t fsuuid;
+	fstype_t fstype;
 	char *storage_url;
 	int use_device;
 	char *devname;
@@ -88,7 +92,7 @@ int vzctl2_set_disk(struct vzctl_env_handle *h, struct vzctl_disk_param *param);
 int vzctl2_resize_disk(struct vzctl_env_handle *h, const char *guid,
 		unsigned long size, int offline);
 int vzctl_setup_disk(struct vzctl_env_handle *h, struct vzctl_env_disk *env_disk, int flags);
-int get_fs_uuid(const char *device, char *uuid);
+int get_fs_uuid(const char *device, struct vzctl_disk *disk);
 int env_fin_configure_disk(struct vzctl_env_disk *disk);
 int fin_configure_disk(struct vzctl_env_handle *h, struct vzctl_env_disk *disk);
 void get_partition_dev_name(dev_t dev, char *out, int len);

lib/disk_configure.c
@@ -47,21 +47,19 @@
 
 struct exec_disk_param {
 	struct vzctl_env_handle *h;
-	const char *fsuuid;
 	struct vzctl_disk *disk;
 	int automount;
 	char *partname;
 	dev_t partdev;
 };
 
-int get_fs_uuid(const char *device, char *uuid)
+int get_fs_uuid(const char *device, struct vzctl_disk *disk)
 {
 	struct stat st;
 	FILE *fp;
 	int n = 0;
 	char *argv[] = {
-		"/sbin/dumpe2fs",
-		"-h",
+		"/usr/sbin/blkid",
 		(char *)device,
 		NULL,
 	};
@@ -75,17 +73,16 @@ int get_fs_uuid(const char *device, char *uuid)
 		return vzctl_err(-1, errno, "Unable to start %s", argv[0]);
 
 	while (fgets(buf, sizeof(buf), fp)) {
-		n = sscanf(buf, "Filesystem UUID: %38s", uuid);
-		if (n == 1)
-			break;
+		n = sscanf(buf, "%*[^:]: UUID=\"%36[^\"]\" TYPE=\"%4[^\"]\"",
+				disk->fsuuid, disk->fstype);
+		if (n == 2) {
+			fclose(fp);
+			return 0;
+		}
 	}
 	fclose(fp);
 
-	if (n != 1)
-		return vzctl_err(-1, 0, "Unable to get file system uuid dev=%s",
-				device);
-
-	return 0;
+	return vzctl_err(-1, 0, "Unable to get file system uuid dev=%s", device);
 }
 
 static int write_fstab_entry(FILE *fp, const char *uuid, const char *mnt, const char *opts)
@@ -388,25 +385,29 @@ static int env_configure_disk(struct exec_disk_param *param)
 			return -1;
 	}
 
-	if (disk->mnt != NULL && param->fsuuid != NULL) {
+	if (disk->mnt != NULL) {
 		if (access(disk->mnt, F_OK))
 			make_dir(disk->mnt, 1);
+
+		if (disk->dmname)
+			goto skip_configure;
 
 		if (is_systemd()) {
 			// skip unit configure for runing CT #PSBM-33596
 			if (param->h->ctx->state == VZCTL_STATE_STARTING &&
-			   	 env_configure_systemd_unit(param->fsuuid,
+			   	 env_configure_systemd_unit(disk->fsuuid,
 						disk->mnt, disk->mnt_opts))
 				return -1;
 		} else {
-			if (env_configure_fstab(param->fsuuid, disk->mnt,
+			if (env_configure_fstab(disk->fsuuid, disk->mnt,
 						disk->mnt_opts))
 				return -1;
 		}
 
-		if (param->automount || disk->dmname) {
+skip_configure:
+		if (param->automount) {
 			const char *partname = get_fs_partname(disk);
-			if (mount(partname, disk->mnt, "ext4", 0, NULL))
+			if (mount(partname, disk->mnt, disk->fstype, 0, NULL))
 				return vzctl_err(-1, errno, "Failed to mount %s %s",
 					partname, disk->mnt);
 		}
@@ -421,9 +422,8 @@ int configure_disk(struct vzctl_env_handle *h, struct vzctl_disk *disk,
 	char partname[PATH_MAX + 1];
 	struct exec_disk_param param = {
 		.h = h,
-		.fsuuid = (flags & VZCTL_RESTORE) || disk->fsuuid[0] == '\0' ? NULL : disk->fsuuid,
 		.disk = disk,
-		.automount = automount,
+		.automount = (flags & VZCTL_RESTORE) ? 0 : automount,
 		.partname = partname,
 		.partdev = get_fs_partdev(disk),
 	};

commit 10a328eda8b4a68bbcc25d1645e9e79db523c233

Failed to start encrypted CT #PSBM-72567 PSBM-72572
broken by commit 634896cd
Failed to mount /dev/mapper/CRYPT-ploop24700p1 /: No such device

The root disk should not be mounted inside CT

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

lib/disk.c
@@ -1488,7 +1488,7 @@ int vzctl_setup_disk(struct vzctl_env_handle *h, struct vzctl_env_disk *env_disk
 		if (disk->enabled == VZCTL_PARAM_OFF)
 			continue;
 
-		int automount = disk->dmname ? 1 : 0;
+		int automount = (disk->dmname && !is_root_disk(disk)) ? 1 : 0;
 
 		ret = do_setup_disk(h, disk, flags, automount);
 		if (ret && is_permanent_disk(disk))