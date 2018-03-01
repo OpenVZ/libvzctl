OpenVZ/libvzctl

commit 972383f42936fdb80c9f3ed1610ca039adcb6a46

Use path to ve.conf for VE_PRIVATE if VEID is not provided
If vzctl2_env_open_conf() used for unregistered CT
$VEID in 'VE_PRIVATE=/vz/provate/$VEID' is not processed.

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

lib/env_config.c
@@ -21,6 +21,7 @@
  *
  */
 
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -31,6 +32,7 @@
 #include <limits.h>
 #include <errno.h>
 #include <assert.h>
+#include <libgen.h>
 
 #include "env.h"
 #include "cpu.h"
@@ -293,8 +295,13 @@ static int add_env_param(struct vzctl_env_handle *h, struct vzctl_env_param *env
 	case VZCTL_PARAM_VE_PRIVATE:
 		ret = parse_str(&env->fs->ve_private_orig, str, replace);
 		if (ret == 0) {
-			free(env->fs->ve_private);
-			env->fs->ve_private = subst_VEID(EID(h), str);
+			if (EMPTY_CTID(h->ctid) && h->conf->fname) {
+				char *t = strdupa(h->conf->fname);
+				ret = xstrdup(&env->fs->ve_private, dirname(t));
+			} else {
+				free(env->fs->ve_private);
+				env->fs->ve_private = subst_VEID(EID(h), str);
+			}
 		}
 		break;
 	case VZCTL_PARAM_TEMPLATE:

commit 43a8d2fecafa8e26b61addb3accd5f80a96ef10c

add_env_param(): NULL pointer dereference #PSBM-81921
broken by commit 972383f42

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

include/libvzctl.h
@@ -50,7 +50,7 @@ typedef char ctid_t[CTID_SIZE];
 #define EMPTY_CTID(a)	(a == NULL || (a[0]) == '\0')
 #define SET_CTID(a, b)						\
 	do {							\
-		if (b != NULL) {				\
+		if ((b) != NULL) {				\
 			strncpy((a), (b), sizeof(ctid_t) - 1);	\
 			(a[sizeof(ctid_t) - 1]) = '\0';		\
 		} else						\

lib/env.h
@@ -54,7 +54,7 @@
 /** Maximum High Availability priority value */
 #define MAXHAPRIO		UINT_MAX
 
-#define EID(h)		(h->ctid)
+#define EID(h)		((h) ? (h)->ctid : "")
 #define IS_OLD_EID(a)	(strlen(a) < 11)
 
 /* internal flags */

lib/env_config.c
@@ -295,7 +295,7 @@ static int add_env_param(struct vzctl_env_handle *h, struct vzctl_env_param *env
 	case VZCTL_PARAM_VE_PRIVATE:
 		ret = parse_str(&env->fs->ve_private_orig, str, replace);
 		if (ret == 0) {
-			if (EMPTY_CTID(h->ctid) && h->conf->fname) {
+			if (h && EMPTY_CTID(EID(h)) && h->conf->fname) {
 				char *t = strdupa(h->conf->fname);
 				ret = xstrdup(&env->fs->ve_private, dirname(t));
 			} else {