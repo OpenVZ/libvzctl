OpenVZ/libvzctl

commit c5782693ef55024afc86cf4b35366fc4f58e8241

Add option to set memguarantee in bytes #PSBM-107403
add VZCTL_MEM_GUARANTEE_BYTES type
struct vzctl_mem_guarantee  g = {
	.type = VZCTL_MEM_GUARANTEE_BYTES,
}

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

lib/env_config.c
@@ -191,6 +191,7 @@ static struct vzctl_config_param config_param_map[] = {
 {"RATEBOUND",	VZCTL_PARAM_RATEBOUND},
 
 {"MEMGUARANTEE",VZCTL_PARAM_MEM_GUARANTEE},
+{"MEMGUARANTEE_BYTES", VZCTL_PARAM_MEM_GUARANTEE_BYTES},
 {"PAGECACHE_ISOLATION",VZCTL_PARAM_PAGECACHE_ISOLATION},
 {"NUMMEMORYSUBGROUPS", VZCTL_PARAM_NUMMEMORYSUBGROUPS},
 {"NUMNETIF", VZCTL_PARAM_NUMNETIF},
@@ -212,16 +213,17 @@ static int parse_str(char **dst, const char *src, int replace)
 	return xstrdup(dst, src);
 }
 
-static int parse_memguar(struct vzctl_res_param *res, const char *str)
+static int parse_memguar(struct vzctl_res_param *res, const char *str,
+		int in_bytes)
 {
-	struct vzctl_mem_guarantee x = {};
+	struct vzctl_mem_guarantee x = {
+		.type = in_bytes ? VZCTL_MEM_GUARANTEE_BYTES :
+				VZCTL_MEM_GUARANTEE_PCT};
 
 	if (strcmp(str, "auto") == 0) {
 		x.type = VZCTL_MEM_GUARANTEE_AUTO;
-	} else {
-		x.type = VZCTL_MEM_GUARANTEE_PCT;
-		if (parse_ul(str, &x.value))
-			return VZCTL_E_INVAL;
+	} else if (parse_ul(str, &x.value)) {
+		return VZCTL_E_INVAL;
 	}
 
 	if (res->memguar == NULL) {
@@ -239,7 +241,7 @@ static char *memguar2str(struct vzctl_mem_guarantee *memguar)
 {
 	char x[12] = "auto";
 
-	if (memguar->type == VZCTL_MEM_GUARANTEE_PCT)
+	if (memguar->type != VZCTL_MEM_GUARANTEE_AUTO)
 		snprintf(x, sizeof(x), "%lu", memguar->value);
 
 	return strdup(x);
@@ -755,9 +757,11 @@ static int add_env_param(struct vzctl_env_handle *h, struct vzctl_env_param *env
 		env->vz->tc->ratebound = id;
 		break;
 	case VZCTL_PARAM_MEM_GUARANTEE:
+	case VZCTL_PARAM_MEM_GUARANTEE_BYTES:
 		if (env->res->memguar != NULL && !replace)
 			break;
-		ret = parse_memguar(env->res, str);
+		ret = parse_memguar(env->res, str,
+				param_id == VZCTL_PARAM_MEM_GUARANTEE_BYTES);
 		break;
 	case VZCTL_PARAM_PAGECACHE_ISOLATION:
 		if ((id = yesno2id(str)) == -1)
@@ -1334,8 +1338,16 @@ static char *env_param2str(struct vzctl_env_handle *h,
 			return strdup(id2yesno(env->vz->tc->ratebound ));
 		break;
 	case VZCTL_PARAM_MEM_GUARANTEE:
-		if (env->res->memguar != NULL)
+		if (env->res->memguar != NULL) {
+			vzctl_conf_del_param(h->conf, "MEMGUARANTEE_BYTES");
 			return memguar2str(env->res->memguar);
+		}
+		break;
+	case VZCTL_PARAM_MEM_GUARANTEE_BYTES:
+		if (env->res->memguar != NULL) {
+			vzctl_conf_del_param(h->conf, "MEMGUARANTEE");
+			return memguar2str(env->res->memguar);
+		}
 		break;
 	case VZCTL_PARAM_PAGECACHE_ISOLATION:
 		if (env->res->ub->pagecache_isolation)

include/libvzctl.h
@@ -537,6 +537,7 @@ struct vzctl_reinstall_param {
 enum {
 	VZCTL_MEM_GUARANTEE_AUTO = 0,
 	VZCTL_MEM_GUARANTEE_PCT,
+	VZCTL_MEM_GUARANTEE_BYTES,
 };
 
 struct vzctl_mem_guarantee {

lib/vcmm.c
@@ -155,7 +155,10 @@ static int get_vcmm_config(struct vzctl_env_handle *h,
 		mem = ub->physpages->l * get_pagesize();
 		mem_p = &mem;
 		/* scale guaranty on memlimit change */
-		if (guar == NULL && guar_bytes_cur != 0) {
+		if (guar == NULL && guar_bytes_cur != 0 &&
+			h->env_param->res->memguar != NULL &&
+			h->env_param->res->memguar->type == VZCTL_MEM_GUARANTEE_PCT)
+		{
 			guar_def.type = VZCTL_MEM_GUARANTEE_PCT;
 			if (mem_cur)
 				guar_def.value = ((float)guar_bytes_cur / mem_cur) * 100;
@@ -169,12 +172,18 @@ static int get_vcmm_config(struct vzctl_env_handle *h,
 	}
 
 	if (guar != NULL) {
-		x = (guar->type == VZCTL_MEM_GUARANTEE_AUTO) ?
-				0 : guar->value;
-
-		guar_bytes = ((float)mem * x) / 100;
-
-		logger(0, 0, "Configure memguarantee: %lu%%", x);
+		switch (guar->type) {
+		case VZCTL_MEM_GUARANTEE_AUTO:
+			guar_bytes = 0;
+			break;
+		case VZCTL_MEM_GUARANTEE_PCT:
+			logger(0, 0, "Configure memguarantee: %lu%%", guar->value);
+			guar_bytes = ((float)mem * guar->value) / 100;
+			break;
+		case VZCTL_MEM_GUARANTEE_BYTES:
+			guar_bytes = guar->value;
+			break;
+		}
 		guar_p = &guar_bytes;
 	}
 

lib/vzctl_param.h
@@ -196,6 +196,7 @@ enum {
 	VZCTL_PARAM_NUMMEMORYSUBGROUPS,
 	VZCTL_PARAM_NUMNETIF,
 	VZCTL_PARAM_NETIF_VPORT_TYPE,
+	VZCTL_PARAM_MEM_GUARANTEE_BYTES,
 };
 
 #endif

commit 6071e5f0263660bc48025d49c98a8f1b4077b06a

fix store MEMGUARANTEE parameter
was broken by commit c5782693

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

lib/env_config.c
@@ -1338,13 +1338,15 @@ static char *env_param2str(struct vzctl_env_handle *h,
 			return strdup(id2yesno(env->vz->tc->ratebound ));
 		break;
 	case VZCTL_PARAM_MEM_GUARANTEE:
-		if (env->res->memguar != NULL) {
+		if (env->res->memguar != NULL &&
+				env->res->memguar->type != VZCTL_MEM_GUARANTEE_BYTES) {
 			vzctl_conf_del_param(h->conf, "MEMGUARANTEE_BYTES");
 			return memguar2str(env->res->memguar);
 		}
 		break;
 	case VZCTL_PARAM_MEM_GUARANTEE_BYTES:
-		if (env->res->memguar != NULL) {
+		if (env->res->memguar != NULL &&
+				env->res->memguar->type == VZCTL_MEM_GUARANTEE_BYTES) {
 			vzctl_conf_del_param(h->conf, "MEMGUARANTEE");
 			return memguar2str(env->res->memguar);
 		}