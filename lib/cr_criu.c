OpenVZ/libvzctl

commit 2fe58473d5636b25d688e6196be9145d8beb8746

scripts/vz-rst-action.in: restore devices #PSBM-106869
Multiple migration issue
restore minor for all devices

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

lib/cr_criu.c
@@ -106,6 +106,13 @@ static int make_ext_mount_args(struct vzctl_env_handle *h, int cpt, char *out,
 	return 0;
 }
 
+static const char *get_basename(const char *path)
+{
+	char *x = strrchr(path, '/');
+
+	return x ? ++x : path;
+}
+
 static int make_ploop_dev_args(struct vzctl_env_handle *h, char *out, int size)
 {
 	char *ep, *pbuf = out;
@@ -121,7 +128,7 @@ static int make_ploop_dev_args(struct vzctl_env_handle *h, char *out, int size)
 
 		pbuf += snprintf(pbuf, ep - pbuf, "%s@%s:%d:%d:%s\n",
 				d->uuid,
-				get_fs_partname(d),
+				get_basename(get_fs_partname(d)),
 				gnu_dev_major(part_dev),
 				gnu_dev_minor(part_dev),
 				is_root_disk(d) ? "root" : "");

scripts/vz-rst-action.in
@@ -31,9 +31,40 @@ exec 1>&2
 
 action_script=@SCRIPTDIR@/vz-rst-action
 
+update_devices()
+{
+	local olddev=$1
+	local newdev=$2
+	local major=$3
+	local minor=$4
+	local omj omr mj mr
+	local dir=${olddev%/*}
+
+	rm -f $newdev
+	mknod -m 660 $newdev b $major $minor
+	[ -e "$olddev" ] || return
+
+	omj=$(stat -c %t $olddev)
+	omj=$(printf "%d" 0x$omj)
+	omr=$(stat -c %T $olddev)
+	omr=$(printf "%d" 0x$omr)
+	for n in `ls -1 $dir`; do
+		mj=$(stat -c %t $dir/$n)
+		mj=$(printf "%d" 0x$mj)
+
+		mr=$(stat -c %T $dir/$n)
+		mr=$(printf "%d" 0x$mr)
+
+		if [ "$mj" = "$omj" -a "$mr" = "$omr" ]; then
+			rm -f $dir/$n
+			mknod -m 660 $dir/$n b $major $minor
+		fi
+	done
+}
+
 restore_devices()
 {
-	local s d t major minor old dir device
+	local s d t major minor old dir device pfx
 	local pid=$CRTOOLS_INIT_PID
 
 	if [ -z "$VZ_RST_USE_NSENTER" ]; then
@@ -42,7 +73,13 @@ restore_devices()
 		exit 0
 	fi
 
-	for root in $CRIU_DEVTMPFS $CRIU_MNT_NS_ROOTS; do
+	if [ -n "$CRIU_DEVTMPFS" ]; then
+		MNT=$CRIU_DEVTMPFS
+	else
+		MNT=$CRIU_MNT_NS_ROOTS
+		pfx=dev
+	fi
+	for root in $MNT; do
 		# VE_PLOOP_DEVS=UUID@ploopN:major:minor:[root]
 		for s in $VE_PLOOP_DEVS; do
 			uuid=${s%%@*}
@@ -53,22 +90,14 @@ restore_devices()
 			t=${t#*:}
 			minor=${t%%:*}
 
-			[ ! -L "$root/dev/$uuid" ] && continue
+			[ ! -L "$root/$pfx/$uuid" ] && continue
 
-			old=$(readlink $root/dev/$uuid)
+			old=$(readlink $root/$pfx/$uuid)
 			[ -z "$old" ] && continue
+			# /dev/ploopXXX -> ploopXXX
+			old=${old##*/}
 
-			for d in "${old}" "${device}"; do
-				dir=$root/$d
-				dir=${dir%/*}
-				if [ -e "$dir" ]; then
-					rm -f $root/$d
-				else
-					mkdir -p $dir
-				fi
-				mknod -m 660 $root/$d b $major $minor
-			done
-
+			update_devices "$root/$pxf/$old" "$root/$pfx/$device" $major $minor
 			rm -f $root/dev/$uuid
 		done
 	done

commit 8f1c8a9a78963aa296d4fc2fbc7c8f081f1af229

fixed restore devices #PSBM-107773
broken by commit 2fe58473 "restore devices #PSBM-106869"

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

lib/cr_criu.c
@@ -106,13 +106,6 @@ static int make_ext_mount_args(struct vzctl_env_handle *h, int cpt, char *out,
 	return 0;
 }
 
-static const char *get_basename(const char *path)
-{
-	char *x = strrchr(path, '/');
-
-	return x ? ++x : path;
-}
-
 static int make_ploop_dev_args(struct vzctl_env_handle *h, char *out, int size)
 {
 	char *ep, *pbuf = out;
@@ -128,7 +121,7 @@ static int make_ploop_dev_args(struct vzctl_env_handle *h, char *out, int size)
 
 		pbuf += snprintf(pbuf, ep - pbuf, "%s@%s:%d:%d:%s\n",
 				d->uuid,
-				get_basename(get_fs_partname(d)),
+				get_fs_partname(d),
 				gnu_dev_major(part_dev),
 				gnu_dev_minor(part_dev),
 				is_root_disk(d) ? "root" : "");

scripts/vz-rst-action.in
@@ -64,7 +64,7 @@ update_devices()
 
 restore_devices()
 {
-	local s d t major minor old dir device pfx
+	local s d t major minor old dir device
 	local pid=$CRTOOLS_INIT_PID
 
 	if [ -z "$VZ_RST_USE_NSENTER" ]; then
@@ -77,7 +77,6 @@ restore_devices()
 		MNT=$CRIU_DEVTMPFS
 	else
 		MNT=$CRIU_MNT_NS_ROOTS
-		pfx=dev
 	fi
 	for root in $MNT; do
 		# VE_PLOOP_DEVS=UUID@ploopN:major:minor:[root]
@@ -85,19 +84,19 @@ restore_devices()
 			uuid=${s%%@*}
 			t=${s#*@}
 			device=${t%%:*}
+			[ -n "$CRIU_DEVTMPFS" ] &&  device=${device#/dev}
 			t=${t#*:}
 			major=${t%%:*}
 			t=${t#*:}
 			minor=${t%%:*}
 
-			[ ! -L "$root/$pfx/$uuid" ] && continue
+			[ ! -L "$root/$uuid" ] && continue
 
-			old=$(readlink $root/$pfx/$uuid)
+			old=$(readlink $root/$uuid)
 			[ -z "$old" ] && continue
-			# /dev/ploopXXX -> ploopXXX
-			old=${old##*/}
+			[ -n "$CRIU_DEVTMPFS" ] && old=${old#/dev}
 
-			update_devices "$root/$pxf/$old" "$root/$pfx/$device" $major $minor
+			update_devices "$root/$pxf/$old" "$root/$device" $major $minor
 			rm -f $root/dev/$uuid
 		done
 	done