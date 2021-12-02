OpenVZ/libvzctl

commit e8120dbfa3ca2439d75079dd399b74197b054d10

Introduce a possibility to create and mount qcow2 images #PSBM-135497
Use VEFTYPE=qcow2 global configuration parameter

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

lib/create.c
@@ -103,7 +103,7 @@ int vzctl2_get_def_ostemplate_name(char *out, int size)
 
 static int is_ploop_cache(const char *name)
 {
-	return strstr(name, ".plain.ploop") != NULL ? 1 : 0;
+	return strstr(name, ".ploop") || strstr(name, ".qcow2") ? 1 : 0;
 }
 
 static int get_def_ve_layout(void)
@@ -116,6 +116,7 @@ static int get_def_ve_layout(void)
 		if (vefstype != VZ_T_VZFS3 &&
 				vefstype != VZ_T_VZFS4 &&
 				vefstype != VZ_T_EXT4 &&
+				vefstype != VZ_T_QCOW2 &&
 				vefstype != VZ_T_SIMFS)
 			goto out;
 
@@ -310,13 +311,25 @@ static int create_private_ploop(const char *dst, unsigned long size,
 
 	switch (layout) {
 	case VZCTL_LAYOUT_5:
+	case VZCTL_LAYOUT_6:
+		if (strstr(tarball, ".qcow2") != NULL) {
+			snprintf(data_root, sizeof(data_root), "%s", dst);
+			break;
+		}
 		get_root_disk_path(dst, data_root, sizeof(data_root));
+		/* Create compatible symlink templates -> root.hdd/templates */
+		snprintf(buf, sizeof(buf), "%s/templates", data_root);
+		if (stat_file(buf) == 1) {
+			snprintf(buf, sizeof(buf), "%s/templates", dst);
+			if (symlink("root.hdd/templates", buf))
+				logger(-1, errno, "Unable to create the symlink: %s", buf);
+		}
 		break;
 	case VZCTL_LAYOUT_4:
 		snprintf(data_root, sizeof(data_root), "%s", dst);
 		break;
 	default:
-		return vzctl_err(VZCTL_E_INVAL, 9, "Unsupported CT layout %d",
+		return vzctl_err(VZCTL_E_INVAL, 0, "Unsupported CT layout %d",
 				layout);
 	}
 
@@ -343,17 +356,8 @@ static int create_private_ploop(const char *dst, unsigned long size,
 	if (layout == VZCTL_LAYOUT_4)
 		return 0;
 
-	/* Create compatible symlink templates -> root.hdd/templates */
-	snprintf(buf, sizeof(buf), "%s/templates", data_root);
-	if (stat_file(buf) == 1) {
-		snprintf(buf, sizeof(buf), "%s/templates", dst);
-		if (symlink("root.hdd/templates", buf))
-			logger(-1, errno, "Unable to create the symlink: %s",
-					buf);
-	}
-
 	if (size)
-		return vzctl2_resize_disk_image(data_root, size, 0);
+		return vzctl2_resize_disk_image(get_root_disk_path(dst, data_root, sizeof(data_root)), size, 0);
 
 	return 0;
 }

include/libvzctl.h
@@ -43,6 +43,7 @@
 #define VZCTL_LAYOUT_3  3
 #define VZCTL_LAYOUT_4  4
 #define VZCTL_LAYOUT_5  5
+#define VZCTL_LAYOUT_6  6
 
 #define VZCTL_CPUUNITS_MAX     500000
 #define VZCTL_CPUUNITS_MIN     8
@@ -97,6 +98,7 @@ enum {
 	VZ_T_VZFS0 = (1<<13),
 	VZ_T_SIMFS = VZ_T_VZFS0,
 	VZ_T_EXT4 = (1<<14),
+	VZ_T_QCOW2 = (1<<15),
 };
 
 enum {

lib/env.c
@@ -630,7 +630,7 @@ static int check_requires(struct vzctl_env_param *env, int flags)
 			"Container private area %s does not exist",
 			env->fs->ve_private);
 
-	if (env->fs->layout == VZCTL_LAYOUT_5 &&
+	if (env->fs->layout >= VZCTL_LAYOUT_5 &&
 			find_root_disk(env->disk) == NULL)
 		return vzctl_err(VZCTL_E_INVAL, 0,
 			"Container root disk is not configured");
@@ -822,7 +822,7 @@ int pre_setup_env(const struct start_param *param)
 			return ret;
 	}
 
-	if (env->fs->layout == VZCTL_LAYOUT_5 && env->disk != NULL &&
+	if (env->fs->layout >= VZCTL_LAYOUT_5 && env->disk != NULL &&
 			!is_secondary_disk_present(env->disk))
 	{
 		env_fin_configure_disk(env->disk);
@@ -1618,7 +1618,7 @@ int vzctl2_apply_param(struct vzctl_env_handle *h, struct vzctl_env_param *env,
 				goto err;
 		}
 
-		if (h->env_param->fs->layout == VZCTL_LAYOUT_5 &&
+		if (h->env_param->fs->layout >= VZCTL_LAYOUT_5 &&
 			env->dq->diskspace != NULL &&
 			(ret = vzctl2_resize_image(h->env_param->fs->ve_private,
 							  env->dq->diskspace->l, 0)))
@@ -3490,7 +3490,7 @@ int vzctl2_env_get_layout(struct vzctl_env_param *env, int *layout)
 
 int vzctl2_env_set_layout(struct vzctl_env_param *env, int layout, int flags)
 {
-	if (layout < VZCTL_LAYOUT_3 || layout > VZCTL_LAYOUT_5)
+	if (layout < VZCTL_LAYOUT_3 || layout > VZCTL_LAYOUT_6)
 		return vzctl_err(VZCTL_E_INVAL, 0, "An invalid layout is specified %d",
 				layout);
 

lib/fs_vzfs.c
@@ -133,7 +133,7 @@ int vzctl2_create_env_private(const char *ve_private, int layout)
 {
 	char path[MAXPATHLEN];
 
-	if (layout < VZCTL_LAYOUT_3 || layout > VZCTL_LAYOUT_5)
+	if (layout < VZCTL_LAYOUT_3 || layout > VZCTL_LAYOUT_6)
 		return vzctl_err(-1, 0, "Unsupported layout %d", layout);
 
 	if (stat_file(ve_private) == 0 && make_dir(ve_private, 1))
@@ -153,7 +153,7 @@ int vzctl2_create_env_private(const char *ve_private, int layout)
 		snprintf(path, sizeof(path), "%s" VZCTL_VE_SCRIPTS_DIR, ve_private);
 		if (make_dir(path, 1))
 			goto err;
-	} else if (layout == VZCTL_LAYOUT_5) {
+	} else if (layout == VZCTL_LAYOUT_5 || layout == VZCTL_LAYOUT_6) {
 		/* VZ 5.0 layout structure */
 		snprintf(path, sizeof(path), "%s" VZCTL_VE_FS_DIR, ve_private);
 		if (make_dir(path, 1))

lib/image.c
@@ -55,15 +55,12 @@ const char *get_root_disk_path(const char *ve_private, char *buf, int len)
 
 int open_dd(const char *path, struct ploop_disk_images_data **di)
 {
-	char fname[PATH_MAX];
-
 	if (path[0] == '\0')
 		return vzctl_err(VZCTL_E_INVAL, 0, "Error: disk image path is empty");
 
-	snprintf(fname, sizeof(fname), "%s/" DISKDESCRIPTOR_XML, path);
-	if (ploop_open_dd(di, fname))
+	if (ploop_open_dd(di, path))
 		return vzctl_err(VZCTL_E_PARSE_DD, 0, "Failed to read %s: %s",
-				fname, ploop_get_last_error());
+				path, ploop_get_last_error());
 	return 0;
 }
 
@@ -102,9 +99,14 @@ int vzctl2_is_image_mounted(const char *path)
 	char fname[PATH_MAX];
 	struct stat st;
 
-	snprintf(fname, sizeof(fname), "%s/" DISKDESCRIPTOR_XML, path);
-	if (stat(fname, &st) && errno == ENOENT)
+	if (stat(path, &st) && errno == ENOENT)
 		return 0;
+
+	if (S_ISDIR(st.st_mode)) {
+		snprintf(fname, sizeof(fname), "%s/" DISKDESCRIPTOR_XML, path);
+		if (stat(fname, &st) && errno == ENOENT)
+			return 0;
+	}
 
 	ret = open_dd(path, &di);
 	if (ret)

lib/snapshot.c
@@ -262,7 +262,7 @@ static int is_snapshot_supported(char *ve_private)
 	int layout;
 
 	layout = vzctl2_env_layout_version(ve_private);
-	if (layout != VZCTL_LAYOUT_5) {
+	if (layout < VZCTL_LAYOUT_5) {
 		logger(-1, 0,"Snapshot supported for ploop based Container only");
 		return 0;
 	}

lib/vzfeatures.c
@@ -62,6 +62,8 @@ int vzctl2_fstype2layout(unsigned long fstype)
 		return VZCTL_LAYOUT_4;
 	case VZ_T_EXT4:
 		return VZCTL_LAYOUT_5;
+	case VZ_T_QCOW2:
+		return VZCTL_LAYOUT_6;
 	default:
 		return 0;
 	}
@@ -70,9 +72,14 @@ int vzctl2_fstype2layout(unsigned long fstype)
 
 const char *vzctl2_layout2fstype(int layout)
 {
-	if (layout == VZCTL_LAYOUT_5)
+	switch (layout) {
+	case VZCTL_LAYOUT_5:
 		return "ext4";
-	return "simfs";
+	case VZCTL_LAYOUT_6:
+		return "qcow2";
+	default:
+		return "simfs";
+	}
 }
 
 static struct feature_s *find_feature(const char *name)
@@ -178,6 +185,7 @@ static struct tech_mtx {
 	{VZ_T_VZFS_MFILES,	"2.6.9"},
 	{VZ_T_SIMFS,		"2.4"},
 	{VZ_T_EXT4,		"2.6.32"},
+	{VZ_T_QCOW2,		"5.14.0"},
 };
 
 static struct id2name {
@@ -196,6 +204,7 @@ static struct id2name {
 	{VZ_T_EXT4,		"ext4"},
 	{VZ_T_VZFS0,		"simfs"},
 	{VZ_T_SIMFS,		"simfs"},
+	{VZ_T_QCOW2,		"qcow2"},
 };
 
 const char *vzctl2_tech2name(unsigned long long id)

commit e44c3db155617916259da2ed195a1fed409806b6

create link to 'templates' folder #PSBM-136348
was broken by e8120dbf

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

lib/create.c
@@ -318,12 +318,9 @@ static int create_private_ploop(const char *dst, unsigned long size,
 		}
 		get_root_disk_path(dst, data_root, sizeof(data_root));
 		/* Create compatible symlink templates -> root.hdd/templates */
-		snprintf(buf, sizeof(buf), "%s/templates", data_root);
-		if (stat_file(buf) == 1) {
-			snprintf(buf, sizeof(buf), "%s/templates", dst);
-			if (symlink("root.hdd/templates", buf))
-				logger(-1, errno, "Unable to create the symlink: %s", buf);
-		}
+		snprintf(buf, sizeof(buf), "%s/templates", dst);
+		if (symlink("root.hdd/templates", buf))
+			logger(-1, errno, "Unable to create the symlink: %s", buf);
 		break;
 	case VZCTL_LAYOUT_4:
 		snprintf(data_root, sizeof(data_root), "%s", dst);